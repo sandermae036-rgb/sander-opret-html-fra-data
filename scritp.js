bf8f416692bd053afe54dbc4799d21162cffe41f
AgentSMDJ <sandermae036@gmail.com>
2026-09-03 12:21:13 +0200

    added array with students

---
+++ scritp.js
@@ -0,0 +1,13 @@
+const students = [
+    {name: "Sofie", age: 25, course: "Webudvikler"},
+    {name: "Lukas", age: 22, course: "Mediegrafiker"},
+    {name: "Emil", age: 28, course: "Teknisk Designer"},
+];
+
+const root = document.querySelector(".idk");
+
+students.forEach(function(student){
+    const divElement = document.createElement("div");
+    divElement.textContent = student.name + ', Alder' + student.age + ', uddannelse: ' + student.course;
+    root.append(divElement);
+});
